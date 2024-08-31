bf2fca8b706fe1283ec44613c98bf3e80c917cd8
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2024-08-31 13:09:49 +0530

    adding the components

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+
+function Header() {
+	const description = reactDescriptions[generateRandomindex(3)]
+	return (
+		<header>
+			{/* now output dynamically for images ->  */}
+			{/* the below apporach the src attribute value point directly my source folder , which leads  image might lost, when we prepare this react project for deployment, when this process happen, image files, when loaded like below, might be ignored by that bundling process ->so better import the images */}
+
+			{/* <img src="src/assets/react-core-concepts.png" alt="Stylized atom" /> */}
+
+			{/* from below apporach , since it's a javascript varaible that will inculde a  path to image -> note: it will include an automatically generated path that will also work once, deploy the React app to a server */}
+
+			<img src={reactImg} alt="React Image"/>
+			<h1>React Essentials</h1>
+			<p>
+				{description} React concepts you will need for almost any app you
+				are going to build!
+			</p>
+		</header>
+	);
+}
+
+export default Header;
