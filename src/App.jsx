5fb8f1499cbf0ed8e8bcc20552470a89c9d91211
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2024-08-31 18:02:41 +0530

    modifed header function

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 
 import reactImg from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header.jsx';
 
 
 
@@ -17,26 +18,7 @@ function generateRandomindex(max_index) {
 	
 }
 
-function Header() {
-	const description = reactDescriptions[generateRandomindex(3)]
-	return (
-		<header>
-			{/* now output dynamically for images ->  */}
-			{/* the below apporach the src attribute value point directly my source folder , which leads  image might lost, when we prepare this react project for deployment, when this process happen, image files, when loaded like below, might be ignored by that bundling process ->so better import the images */}
 
-			{/* <img src="src/assets/react-core-concepts.png" alt="Stylized atom" /> */}
-
-			{/* from below apporach , since it's a javascript varaible that will inculde a  path to image -> note: it will include an automatically generated path that will also work once, deploy the React app to a server */}
-
-			<img src={reactImg} alt="React Image"/>
-			<h1>React Essentials</h1>
-			<p>
-				{description} React concepts you will need for almost any app you
-				are going to build!
-			</p>
-		</header>
-	);
-}
 
 
 // concept of Props : configuring components with 'props'
@@ -97,8 +79,6 @@ function CoreConcept({image, title, description}){
 		</li>
 	);
 }
-
-
 
 
 function App() {
