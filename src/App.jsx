475b80ac6860f828a7a42b8372624d0566b835d7
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2024-08-31 10:07:08 +0530

    improve the img src attribute dynamically, which helps in the deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+// import 
+
+import reactImg from './assets/react-core-concepts.png';
+
+
+
+
 // for dynamic output , we use curly braces like object {} 
 // Note : In Curly braces , only use immediate result expression -> don't use for-loops,if-statements,functions
 
@@ -14,7 +21,14 @@ function Header() {
 	const description = reactDescriptions[generateRandomindex(3)]
 	return (
 		<header>
-			<img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+			{/* now output dynamically for images ->  */}
+			{/* the below apporach the src attribute value point directly my source folder , which leads  image might lost, when we prepare this react project for deployment, when this process happen, image files, when loaded like below, might be ignored by that bundling process ->so better import the images */}
+
+			{/* <img src="src/assets/react-core-concepts.png" alt="Stylized atom" /> */}
+
+			{/* from below apporach , since it's a javascript varaible that will inculde a  path to image -> note: it will include an automatically generated path that will also work once, deploy the React app to a server */}
+
+			<img src={reactImg} alt="React Image"/>
 			<h1>React Essentials</h1>
 			<p>
 				{description} React concepts you will need for almost any app you
