8b1a8093e6bfcfb49a17766edbb1f5faccf0d9d0
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2024-08-31 09:40:43 +0530

    added header and outputting dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+// for dynamic output , we use curly braces like object {} 
+// Note : In Curly braces , only use immediate result expression -> don't use for-loops,if-statements,functions
+
+const reactDescriptions = ['Fundamental', "Core", "Essential", "Curical"]
+
+// generate random indexs
+
+function generateRandomindex(max_index) {
+	return Math.floor(Math.random() * (max_index + 1));
+	
+}
+
 function Header() {
 	return (
 		<header>
 			<img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
 			<h1>React Essentials</h1>
 			<p>
-				Fundamental React concepts you will need for almost any app you
+				{reactDescriptions[generateRandomindex(3)]} React concepts you will need for almost any app you
 				are going to build!
 			</p>
 		</header>
