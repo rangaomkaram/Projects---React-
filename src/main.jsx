48ecea95fb6fadbf84d797c73a4b617d74fd8948
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2024-09-20 14:18:50 +0530

    adding the concept of without jsx -> using createElement

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css';
+import React from 'react';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+//  createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+// here below code instead jsx code we used createElement function using react , which has props to accept tags as objects, child content 
+createRoot(document.getElementById('root')).render(React.createElement(App))
